28f8678d791193d1514deaf73c5bf0f10d1dd310
Christina Kiesz <ckiesz@gmail.com>
2025-05-13 11:25:49 -0700

    install shadcn for components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 
 import './App.css'
+import { Card } from './components/ui/card'
 
 function App() {
 
   return (
     <>
+		<Card>
       <h1 className='font-bold text-blue-800'>Bakeshop</h1>
+			</Card>
     </>
   )
 }
