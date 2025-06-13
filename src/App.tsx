670d45b9c2c81f62ce281ac934d2b56c8775e60d
Christina Kiesz <ckiesz@gmail.com>
2025-06-12 17:19:55 -0700

    add storybook and configure

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 
 import './App.css'
 import { Card } from './components/ui/card'
+import { ThemeProvider } from "@/components/theme-provider"
 
 function App() {
 
   return (
-    <>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
 		<Card>
       <h1 className='font-bold text-blue-800'>Bakeshop</h1>
 			</Card>
-    </>
+    </ThemeProvider>
   )
 }
 
